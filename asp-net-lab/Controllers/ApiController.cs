4f2348ffc660c4a9406e2dc2ea692bf6639f07c9
Jarosław Żak <shado.ackerly@gmail.com>
2020-11-14 13:43:02 +0100

    second task

---
+++ asp-net-lab/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using asp_net_lab.Models;
+
+namespace asp_net_lab.Controllers
+{
+    [Route("api/company")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        public CompanyModel Post(CompanyModel companyModel)
+        {
+            return companyModel;
+        }
+    }
+}
