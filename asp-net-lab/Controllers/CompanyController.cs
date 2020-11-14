e7efa60d8d8ea4c3cee975a0f26104138b12be8d
Jarosław Żak <shado.ackerly@gmail.com>
2020-11-14 12:46:57 +0100

    initial + first task

---
+++ asp-net-lab/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using asp_net_lab.Models;
+
+namespace asp_net_lab.Controllers
+{
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompanyModel model)
+        {
+            var viewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharsInName = model.Name.Length,
+                NumberOfCharsInDescription = model.Description.Length,
+                IsHidden = !model.IsVisible
+            };
+
+            return View("CompanyAdded", viewModel);
+        }
+    }
+}
